346806015785b90dc888f3366d1e79701f6a8f57
mannygarcia98 <mannysg88@gmail.com>
2022-06-02 10:23:24 -0500

    set up CRUD operations for comments

--- public/assets/js/pizza-list.js
+++ public/assets/js/pizza-list.js
@@ -1,4 +1,15 @@
-const $pizzaList = document.querySelector('#pizza-list');
+const $pizzaList = document.querySelector("#pizza-list");
+
+const getPizzaList = () => {
+  fetch("/api/pizzas")
+    .then((response) => response.json())
+    .then((pizzaListArr) => {
+      pizzaListArr.forEach(printPizza);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
 
 const printPizza = ({ _id, pizzaName, toppings, size, commentCount, createdBy, createdAt }) => {
   const pizzaCard = `
@@ -13,10 +24,10 @@ const printPizza = ({ _id, pizzaName, toppings, size, commentCount, createdBy, c
           <h5 class="text-dark">Toppings</h5>
           <ul>
             ${toppings
-              .map(topping => {
+              .map((topping) => {
                 return `<li>${topping}</li>`;
               })
-              .join('')}
+              .join("")}
           </ul>
           <a class="btn display-block w-100 mt-auto" href="/pizza?id=${_id}">See the discussion.</a>
         </div>
@@ -26,3 +37,5 @@ const printPizza = ({ _id, pizzaName, toppings, size, commentCount, createdBy, c
 
   $pizzaList.innerHTML += pizzaCard;
 };
+
+getPizzaList();
